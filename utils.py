b9dd1857bd39e91ced4fb1d33f4f5f461882da88
andras <reka@mollia.hu>
2023-08-24 13:37:45 +0300

    add coupon code generation to utils

--- utils.py
+++ utils.py
@@ -1,12 +1,25 @@
 from config import SessionLocal
 from datetime import date
 from models import Partner, Coupon
+from schemas import CouponCreate
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import random
+import string
+
+CODE_LENGTH = 10
+
+def generate_code() -> str:
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
 
 def valid_partner(partner: Partner) -> bool: 
     return True
 
 def valid_coupon(coupon: Coupon) -> bool: 
     return not coupon.used and coupon.valid_until >= date.today()
+
+def calc_date(cc: CouponCreate) -> date:
+    return date.today() + relativedelta(months=cc.validity_month)
 
 def get_db():
     db = SessionLocal()
