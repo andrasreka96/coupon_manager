e66e32b220d5908c3173dd2fdb21004a656ce110
andras <reka@mollia.hu>
2023-08-24 13:36:04 +0300

    add partner and coupon name select by id

--- crud.py
+++ crud.py
@@ -7,7 +7,6 @@ def create_coupon(db: Session, coupon: Coupon) -> Coupon:
     db.refresh(coupon)
     return coupon
 
-
 def create_partner(db: Session, partner: Partner) -> Partner:
     db.add(partner)
     db.commit()
@@ -20,7 +19,7 @@ def retrieve_partner_by_name(db: Session, name: str) -> Partner:
 def retrieve_coupontype_by_name(db: Session, name: str) -> Session:
     return db.query(CouponType).filter_by(name = name).first()
 
-def retrieve_coupon(db: Session, id: int) -> Coupon: 
+def retrieve_coupon(db: Session, id: str) -> Coupon: 
     return db.query(Coupon).filter_by(id = id).one_or_none()
 
 def update_coupon_used_field(db: Session, coupon: Coupon) -> None: 
@@ -29,4 +28,11 @@ def update_coupon_used_field(db: Session, coupon: Coupon) -> None:
     db.add(coupon)
     db.commit()
     db.refresh(coupon)
+    
+def retrieve_coupontype_by_id(db: Session, id: int) -> CouponType:
+    return db.query(CouponType).filter_by(id = id).one_or_none()
+
+def retrieve_partner_by_id(db: Session, id: int) -> Partner:
+    return db.query(Partner).filter_by(id = id).one_or_none()
+
 
